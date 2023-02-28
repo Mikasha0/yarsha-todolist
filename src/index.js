5c9756e0f16ddaf7b16f598ced32fda277942ef4
Mikasha0 <anikettamrakar0@gmail.com>
2023-02-28 12:22:12 +0545

    first commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+class ToDoList {
+  constructor(todos) {
+    this.todos = todos;
+    this.todoArray = [];
+  }
+  removeTodo(index) {
+    this.todos.splice(index, 1);
+  }
+}
+
+const todoForm = document.querySelector("#todoForm");
+const todoInput = document.querySelector("#new-task");
+var myDiv = document.getElementById("my-div");
+
+const toDoList = new ToDoList([]);
+
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  toDoList.todos.push(todoInput.value);
+
+  renderArray();
+  console.log(toDoList);
+  todoForm.reset();
+});
+
+function renderArray() {
+  myDiv.innerHTML = "";
+
+  toDoList.todos.forEach((item, index) => {
+    const newItem = document.createElement("p");
+    newItem.innerText = item;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+
+    deleteButton.addEventListener("click", () => {
+      toDoList.removeTodo(index);
+      renderArray();
+    });
+
+    newItem.appendChild(deleteButton);
+
+    myDiv.appendChild(newItem);
+  });
+}
